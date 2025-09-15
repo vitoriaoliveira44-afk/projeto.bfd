b71a8bd926f2ae61de5691395f7a44ee0ff88b39
vitoriaoliveira44-afk <vitoria.martinsoliveira44@gmail.com>
2025-09-15 10:33:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+/*
+let entrada =10;
+for (let index = 0; index <= 10 ;index++ ) {
+
+
+   // console.log(`${index} x ${entrada} = ${index * entrada}`);
+
+    
+}
+let i = 0;
+while(i<=5) {
+    console.log(i);
+    i++;
+}
+
+*/
+/*let soma= 0;
+let contador = 1
+while(contador <= 100) {
+    soma+= contador;
+    contador++;
+    console.log(soma);
+}
+
+
+
+let entrada = 20;
+for (let index = 0; index < 20 ;index++ ) {
+    console.log(`${index+1}`)
+
+
+};
+let numero= 2;
+while(numero <=20) {
+    console.log(numero);
+    numero+=2;
+}
+
+let x = 1;
+do{
+    console.log("seja bem vindo" + x);
+    x++;
+}while(x <= 5);
+
+let i = 1;
+while (i <= 10) {
+    console.log("9 x " + i + " = " + (9 * i));
+    i++;
+}
+
+globalThis.saudação = "olá";
+console.log("saudação");
+console.log(globalThis.saudação);
+
+/*function boasVindas (Maria,Dra) {
+    if(Dra)
+        saudação+= titulo;
+        return`${ola},${Maria}`;
+    }
+console.log(boasVindas("Maria"));
+console.log("Maria","Dra");*/
+
+
+
+/*let frutas = ["abacaxi","melao","banana"]
+console.log(frutas.length)*/
+
+/*let listaDeCompras=["-arroz","-feijao","-açucar","-farinha"]
+for( i=0;i<listaDeCompras.length;i++){
+    console.log(`${listaDeCompras[i]}`);
+}*/
+
+/*function Soma (numero1,numero2){
+    return numero1+numero2 ;
+}
+console.log (Soma(3,4))*/
+
+/*function umNumeropar (numero) {
+if(28 % 2 === 0) {
+    return true;
+} else { 
+    return false;
+  }
+} console.log(umNumeropar(28))*/
+
+/*function Calcularmedia (nota1,nota2,nota3){
+    let media= (10,9,8)/3;
+    return media;
+}
+console.log(Calcularmedia(10,6,4));*/
+
+/*let Compras =["arroz","feijão","açúcar","farinha","macarrão"]
+console.log("Compras:");
+for(let i=0;i<Compras.length;i++){
+    console.log((i+1) +"-"+ Compras[i]);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
